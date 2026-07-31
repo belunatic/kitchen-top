bcef90e5b7555f3840887ff6e63c653ae6b02ff9
belunatic <abelluvyou@yahoo.com>
2026-07-31 15:59:56 -0700

    rename the next-config from ts extension to mjs one; also updated the tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-	/* config options here */
+const nextConfig = {
 	reactStrictMode: true,
 
-	/* Setting up Content Security Policy (CSP) headers for security */
 	async headers() {
 		return [
 			{
