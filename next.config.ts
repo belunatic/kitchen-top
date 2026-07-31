9e46dd5f0d691face4e39e9ae09b23f2b69fb0fb
belunatic <abelluvyou@yahoo.com>
2026-07-31 15:07:53 -0700

    add CSP to overwrite the Vercel inject CSP, allowing inline script that is created by github

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,28 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+
+	/* Setting up Content Security Policy (CSP) headers for security */
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' https://github.githubassets.com https://github.com;
+              connect-src 'self' https://github.com https://api.github.com;
+              img-src 'self' https://avatars.githubusercontent.com data:;
+              style-src 'self' 'unsafe-inline';
+              frame-src https://github.com;
+            `,
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
