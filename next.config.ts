b2c0e7d3d48d496deb01b17be34118ed4769ba89
belunatic <abelluvyou@yahoo.com>
2026-07-31 15:18:01 -0700

    consilidate value to one liner in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,8 @@ const nextConfig: NextConfig = {
 				headers: [
 					{
 						key: "Content-Security-Policy",
-						value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' https://github.githubassets.com https://github.com;
-              connect-src 'self' https://github.com https://api.github.com;
-              img-src 'self' https://avatars.githubusercontent.com data:;
-              style-src 'self' 'unsafe-inline';
-              frame-src https://github.com;
-            `,
+						value:
+							"default-src 'self'; script-src 'self' 'unsafe-inline' https://github.githubassets.com https://github.com; connect-src 'self' https://github.com https://api.github.com; img-src 'self' https://avatars.githubusercontent.com data:; style-src 'self' 'unsafe-inline'; frame-src https://github.com;",
 					},
 				],
 			},
