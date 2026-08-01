856106e434b223f302d080693d1397e078814716
belunatic <abelluvyou@yahoo.com>
2026-08-01 14:23:39 -0700

    added script-src-elem to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
 							"script-src 'self' 'unsafe-inline';",
 							"connect-src 'self' https://github.com https://api.github.com;",
 							"img-src 'self' https://avatars.githubusercontent.com data:;",
+							"script-src-elem 'self' 'unsafe-inline';",
 							"style-src 'self' 'unsafe-inline';",
 							"form-action 'self' https://github.com;", // Crucial for OAuth redirects
 							"frame-src 'self' https://github.com;",
